83d2ce3e56c784cbc2213ac9cadc320b2bf1a896
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-27 11:56:36 +0200

    second test

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -15,9 +15,23 @@ describe("CartParser validation method", () => {
         expect(validate(content)).toEqual([]);
     });
 
-    // it('Not expected name header in file.csv', () => {
-    //
-    // });
+    let wrongHeader = 'Product name,Price,Quantit\n' +
+        'Mollis consequat,9.00,2\n' +
+        'Tvoluptatem,10.32,1\n' +
+        'Scelerisque lacinia,18.90,1\n' +
+        'Consectetur adipiscing,28.72,10\n' +
+        'Condimentum aliquet,13.90,1';
+
+    let err = {
+        "column": 2,
+        "message": "Expected header to be named \"Quantity\" but received Quantit.",
+        "row": 0,
+        "type": "header",
+    };
+
+    it('Not expected name header in file.csv', () => {
+        expect(validate(wrongHeader)).toEqual([err]);
+    });
     //
     // it('Test appropriate amount of cells', () => {
     //
