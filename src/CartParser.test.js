0737938750fb6c00b31f6f740e22f20238d25c69
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-27 12:57:14 +0200

    all errors at once

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -15,9 +15,22 @@ describe("CartParser validation method tests", () => {
         expect(validate(content)).toEqual([]);
     });
 
+    it('Should return err if header undefined', () => {
+        // csv file content with wrong header
+        let wrongHeader = 'Product name,Price\n' +
+            'Condimentum aliquet,13.90,1';
+
+        // err that should be expected
+        let err = {
+            "column": 2,
+            "message": "Expected header to be named \"Quantity\" but received undefined.",
+            "row": 0,
+            "type": "header",
+        };
+        expect(validate(wrongHeader)).toEqual([err]);
+    });
 
     it('Should return err if there is not expected header', () => {
-
         // csv file content with wrong header
         let wrongHeader = 'Product name,Price,Quantit\n' +
             'Condimentum aliquet,13.90,1';
@@ -68,6 +81,46 @@ describe("CartParser validation method tests", () => {
             "type": "cell",
         };
         expect(validate(wrongContent)).toEqual([err]);
+    });
+
+    it('Should return all possible err on wrong csv file ', () => {
+        let wrongContent = 'Product name,Quantit\n' +
+            'Mollis consequat,2\n' +
+            'Tvoluptatem,10.32,-1\n' +
+            ',18.90,1';
+        let err = [
+            {
+                "column": 1,
+                "message": "Expected header to be named \"Price\" but received Quantit.",
+                "row": 0,
+                "type": "header",
+            },
+            {
+                "column": 2,
+                "message": "Expected header to be named \"Quantity\" but received undefined.",
+                "row": 0,
+                "type": "header",
+            },
+            {
+                "column": -1,
+                "message": "Expected row to have 3 cells but received 2.",
+                "row": 1,
+                "type": "row",
+            },
+            {
+                "column": 2,
+                "message": "Expected cell to be a positive number but received \"-1\".",
+                "row": 2,
+                "type": "cell",
+            },
+            {
+                "column": 0,
+                "message": "Expected cell to be a nonempty string but received \"\".",
+                "row": 3,
+                "type": "cell",
+            },
+        ];
+        expect(validate(wrongContent)).toEqual(err);
     });
 });
 
